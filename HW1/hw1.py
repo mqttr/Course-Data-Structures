e034fad338c462c4853ddf9e6008ace9ba3768c9
Matthew <matthew.roland56@gmail.com>
2023-09-12 19:45:45 -0500

    Added Stack implementation

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -2,7 +2,6 @@
 #ID:    
 #Email: mroland@unomaha.edu 	
 
-
 # You are not allowed to modify this class
 class Node:
 	def __init__(self, data):
@@ -21,7 +20,7 @@ class LinkedList:
 
 	def display(self):
 		## variable for iteration
-		temp_node = self.head
+		temp_node: Node = self.head
 
 		## iterating until we reach the end of the linked list
 		while temp_node != None:
@@ -40,7 +39,10 @@ class LinkedList:
 	# You can change the return values (from void to any) for each function as you want 
 	# you can add functions as you want 
 	def sortedAdd(self, value):
-		pass
+		newNode: Node = Node(value)
+            
+		
+        
 
 	def remove(self, idx):
 		pass
@@ -52,12 +54,88 @@ class LinkedList:
 
 	# print linkedlist in a reversed order
 	def printReversedList(self):
-		pass
-
+		s = Stack()
+        
 
 	# Extra functions
 	def add(self, value):
 		pass
+
+class Element():
+    def __init__(self, data: any, prev):
+        self.__data = data
+        self.__prev = prev
+
+    def view_element(self):
+        return self.__data
+    
+    def previous_element(self):
+        return self.__prev
+    
+    # def __str__(self):
+    #     return str(self.__data)
+
+class Stack():
+    def __init__(self):
+        __floor = Element(None, None)
+        self.__top_element: Element = __floor
+
+    def push(self, data: any) -> bool:
+        '''
+        Adds data to stack.
+        '''
+        # try:
+        #     self.stack.append(value)
+        #     return True
+        # except:
+        #     return False
+        
+        newElement = Element(data, self.__top_element)
+        self.__top_element = newElement
+        return
+
+    def pop(self):
+        '''
+        Pops and returns value from stack. Returns None if stack empty
+        '''
+        value = self.__top_element.view_element()
+        self.__top_element = self.__top_element.previous_element()
+        return value
+
+    def dump(self):
+        '''
+        Empties stack and returns generator of the stack
+        '''
+        while self.__top_element.previous_element():
+            yield self.pop()
+        return
+
+    def top(self):
+        return self.__top_element.view_element()
+
+    def is_empty(self):
+        if self.__top_element.previous_element() == None and self.__top_element.view_element() == None: 
+            return True # Previous element is None 
+        else: 
+            return False # Previous element is not None (ie: floor)
+    
+    def is_full(self):
+        return not self.is_empty()
+    
+
+
+    # Non-stack-esque functions
+    def peek_stack(self):
+        '''
+        Seeks through the stack and returns a generator.
+        '''
+        ele = self.__top_element
+        while ele.previous_element():
+            yield ele.view_element()
+            ele = ele.previous_element()
+        
+        return 
+
 
 
 
